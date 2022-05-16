7f3f6025ef11e048fc12b7b3428b6f119f80372a
AmalJob <amajobm@gmail.com>
2022-05-16 21:10:40 +0530

    database connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function (done) {
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://amaljob:adam1direction@cluster0.lq4aw.mongodb.net/shopping?retryWrites=true&w=majority'
     const dbname='shopping'
 
     mongoClient.connect(url,(err,data)=>{
